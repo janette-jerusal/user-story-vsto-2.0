42aea9988543e117a66ea9763c2544f91ad59804
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-08 21:44:54 -0400

    Update ThisAddIn.cs

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -1,1 +1,35 @@
 // ThisAddIn class implementation
+using System;
+using Microsoft.Office.Tools.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace UserStorySimilarityAddIn
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            // This will trigger when the add-in starts
+            System.Diagnostics.Debug.WriteLine("UserStorySimilarityAddIn loaded.");
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Optional: Add cleanup code here if needed
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
