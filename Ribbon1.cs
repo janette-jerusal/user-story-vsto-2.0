7eeaf2af9dc87dc378ff9864f05569c3a50d1571
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-08 21:43:05 -0400

    Update Ribbon1.cs

--- Ribbon1.cs
+++ Ribbon1.cs
@@ -1,80 +1,44 @@
 // Ribbon1 UI logic
-// Ribbon1.Designer.cs
+using System;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace UserStorySimilarityAddIn
 {
-    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    public partial class Ribbon1 : RibbonBase
     {
-        private System.ComponentModel.IContainer components = null;
-
-        public Ribbon1()
-            : base(Globals.Factory.GetRibbonFactory())
+        public Ribbon1() : base(Globals.Factory.GetRibbonFactory())
         {
             InitializeComponent();
         }
 
-        protected override void Dispose(bool disposing)
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
         {
-            if (disposing && (components != null))
+        }
+
+        private void compareButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Workbook workbook = Globals.ThisAddIn.Application.ActiveWorkbook;
+
+            if (workbook == null)
             {
-                components.Dispose();
+                MessageBox.Show("No active workbook.");
+                return;
             }
-            base.Dispose(disposing);
-        }
 
-        #region Component Designer generated code
+            Excel.Sheets sheets = workbook.Sheets;
+            if (sheets.Count < 2)
+            {
+                MessageBox.Show("Workbook must have at least 2 sheets.");
+                return;
+            }
 
-        private void InitializeComponent()
-        {
-            this.tab1 = this.Factory.CreateRibbonTab();
-            this.group1 = this.Factory.CreateRibbonGroup();
-            this.compareButton = this.Factory.CreateRibbonButton();
-            this.tab1.SuspendLayout();
-            this.group1.SuspendLayout();
-            // 
-            // tab1
-            // 
-            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
-            this.tab1.Groups.Add(this.group1);
-            this.tab1.Label = "MyRibbon";
-            this.tab1.Name = "tab1";
-            // 
-            // group1
-            // 
-            this.group1.Items.Add(this.compareButton);
-            this.group1.Label = "My Group";
-            this.group1.Name = "group1";
-            // 
-            // compareButton
-            // 
-            this.compareButton.Label = "Compare";
-            this.compareButton.Name = "compareButton";
-            this.compareButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.compareButton_Click);
-            // 
-            // Ribbon1
-            // 
-            this.Name = "Ribbon1";
-            this.RibbonType = "Microsoft.Excel.Workbook";
-            this.Tabs.Add(this.tab1);
-            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
-            this.tab1.ResumeLayout(false);
-            this.tab1.PerformLayout();
-            this.group1.ResumeLayout(false);
-            this.group1.PerformLayout();
-        }
+            Excel.Worksheet sheet1 = sheets[1];
+            Excel.Worksheet sheet2 = sheets[2];
 
-        #endregion
-
-        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
-        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
-        internal Microsoft.Office.Tools.Ribbon.RibbonButton compareButton;
-    }
-
-    partial class ThisRibbonCollection
-    {
-        internal Ribbon1 Ribbon1
-        {
-            get { return this.GetRibbon<Ribbon1>(); }
+            // Placeholder logic — replace with actual comparison logic
+            MessageBox.Show($"Comparing Sheet1: {sheet1.Name} with Sheet2: {sheet2.Name}");
         }
     }
 }
