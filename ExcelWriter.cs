afc895764387776cdb128d4b801e45082de94af9
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-08 21:47:46 -0400

    Update ExcelWriter.cs

--- ExcelWriter.cs
+++ ExcelWriter.cs
@@ -1,1 +1,40 @@
 // Logic to write to Excel
+using System;
+using System.Data;
+using System.IO;
+using OfficeOpenXml;
+
+namespace UserStorySimilarityAddIn
+{
+    public static class ExcelWriter
+    {
+        public static void WriteDataTableToExcel(DataTable dataTable, string filePath)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Similarity Results");
+
+                // Add headers
+                for (int col = 0; col < dataTable.Columns.Count; col++)
+                {
+                    worksheet.Cells[1, col + 1].Value = dataTable.Columns[col].ColumnName;
+                }
+
+                // Add data
+                for (int row = 0; row < dataTable.Rows.Count; row++)
+                {
+                    for (int col = 0; col < dataTable.Columns.Count; col++)
+                    {
+                        worksheet.Cells[row + 2, col + 1].Value = dataTable.Rows[row][col];
+                    }
+                }
+
+                // Save
+                var fileInfo = new FileInfo(filePath);
+                package.SaveAs(fileInfo);
+            }
+        }
+    }
+}
